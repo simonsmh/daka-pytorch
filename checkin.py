5c4480c5e556c3969e980abe5d9d8c3fa095f7c9
Simon Shi <simonsmh@gmail.com>
2020-02-07 01:37:49 +0800

    Refine checkin

--- checkin.py
+++ checkin.py
@@ -1,9 +1,22 @@
-import torch
+import os
+import sys
+import time
+
 import numpy as np
-from model import ResidualBlock, ResNet
-from utils.utils import LabeltoStr, StrtoLabel, logger
+import requests
+import torch
+from bs4 import BeautifulSoup
 from PIL import Image
+
 from dataset import transform
+from model import ResidualBlock, ResNet
+from utils.utils import LabeltoStr, logger
+
+
+DK_URL = "https://dk.shmtu.edu.cn/"
+CAS_URL = "https://cas.shmtu.edu.cn/"
+CAPTCHA_URL = CAS_URL + "cas/captcha"
+CHECKIN_URL = DK_URL + "checkin"
 
 model = ResNet(ResidualBlock).to("cpu")
 model.eval()
@@ -23,6 +36,81 @@ def detect(Img):
     return predict_label
 
 
+# https://github.com/airstone42/daka/blob/master/main.py
+def login():
+    soup = BeautifulSoup(r.content, "lxml")
+    captcha = s.get(CAPTCHA_URL, stream=True)
+    valid_code = detect(Image.open(captcha.raw))
+    execution = soup.find("input", attrs={"type": "hidden", "name": "execution"})[
+        "value"
+    ]
+    data = {
+        "username": USERNAME,
+        "password": PASSWORD,
+        "validateCode": valid_code,
+        "execution": execution,
+        "_eventId": "submit",
+        "geolocation": "",
+    }
+    post = s.post(r.url, data=data)
+    soup = BeautifulSoup(post.content, "lxml")
+    logger.info("Checkin login: Login...")
+    return (
+        (False, post)
+        if soup.find("div", attrs={"class": "alert alert-danger"})
+        else (True, post)
+    )
+
+
+def checkin():
+    if r.url != DK_URL:
+        return False
+    soup = BeautifulSoup(r.content, "lxml")
+    check = str(soup.find("div", attrs={"class": "form-group"}))
+    flag = False
+    if "Health report have not been submitted today" in check:
+        flag = False
+    if "Health report already submitted" in check:
+        flag = True
+    if not flag:
+        data = {
+            "xgh": USERNAME,
+            "region": 1,
+            "rylx": 4,
+            "status": 0,
+        }
+        post = s.post(CHECKIN_URL, data=data)
+        logger.info("Checkin: Checkin...")
+        soup = BeautifulSoup(post.content, "lxml")
+        if "Health report already submitted" in str(
+            soup.find("div", attrs={"class": "form-group"})
+        ):
+            flag = True
+    if flag:
+        logger.info("Checkin: Checkin successful!")
+    return flag
+
+
 if __name__ == "__main__":
-    I = Image.open("data/valid/00_1.jpg")
-    print(detect(I))
+    USERNAME = sys.argv[1] if len(sys.argv) >= 2 else input("Input username:")
+    PASSWORD = sys.argv[2] if len(sys.argv) >= 3 else input("Input password:")
+    retry_count = 5
+
+    s = requests.Session()
+    r = s.get(DK_URL)
+    for i in range(retry_count):
+        result, r = login()
+        if result:
+            break
+        else:
+            logger.warning(f"Checkin login: {i} Try")
+        time.sleep(5)
+
+    for i in range(retry_count):
+        result = checkin()
+        if result:
+            break
+        else:
+            logger.warning(f"Checkin checkin: {i} Try")
+        time.sleep(5)
+
