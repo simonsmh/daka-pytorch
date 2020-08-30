96c6692297088d442b9fda9b9306bb293ab9ad03
Simon Shi <simonsmh@gmail.com>
2020-08-30 14:58:22 +0800

    take apscheduler for cron

--- checkin.py
+++ checkin.py
@@ -1,9 +1,12 @@
+import json
 import os
 import sys
 
+import httpx
 import numpy as np
-import requests
 import torch
+import uvloop
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bs4 import BeautifulSoup
 from PIL import Image
 
@@ -21,86 +24,122 @@ model.eval()
 model.load_state_dict(torch.load("model/best.pkl", map_location=device))
 
 
-def detect(Img):
-    logger.info(f"Detect: Detecting...")
-    i = transform(Img).unsqueeze(0)
-    predict_label1, predict_label2 = model(i)
-    predict_label = LabeltoStr(
-        [
-            np.argmax(predict_label1.data.numpy()[0]),
-            np.argmax(predict_label2.data.numpy()[0]),
-        ]
-    )
-    logger.info(f"Detect: Result {predict_label}")
-    return predict_label
+async def user(username, password, region, retry_count=5):
+    async def login(client, username, password):
+        def detect(Img):
+            logger.info(f"Detect: Detecting...")
+            i = transform(Img).unsqueeze(0)
+            predict_label1, predict_label2 = model(i)
+            predict_label = LabeltoStr(
+                [
+                    np.argmax(predict_label1.data.numpy()[0]),
+                    np.argmax(predict_label2.data.numpy()[0]),
+                ]
+            )
+            logger.info(f"Detect: Result {predict_label}")
+            return predict_label
 
+        home = await client.get(DK_URL)
+        soup = BeautifulSoup(home.content, "lxml")
+        captcha = await client.get(CAPTCHA_URL)
+        valid_code = detect(Image.open(captcha))
+        execution = soup.find("input", attrs={"type": "hidden", "name": "execution"})
+        data = {
+            "username": username,
+            "password": password,
+            "validateCode": valid_code,
+            "execution": execution.get("value"),
+            "_eventId": "submit",
+            "geolocation": "",
+        }
+        post = await client.post(home.url, data=data)
+        logger.info(f"Login: {username} Login...")
+        return True if post.url == DK_URL else False
 
-# https://github.com/airstone42/daka/blob/master/main.py
-def login():
-    home = s.get(DK_URL)
-    soup = BeautifulSoup(home.content, "lxml")
-    captcha = s.get(CAPTCHA_URL, stream=True)
-    valid_code = detect(Image.open(captcha.raw))
-    execution = soup.find("input", attrs={"type": "hidden", "name": "execution"})
-    data = {
-        "username": USERNAME,
-        "password": PASSWORD,
-        "validateCode": valid_code,
-        "execution": execution.get("value"),
-        "_eventId": "submit",
-        "geolocation": "",
+    async def checkin(client, username, region):
+        data = {
+            "xgh": username,
+            "lon": "",
+            "lat": "",
+            "region": region,
+            "rylx": 4,
+            "status": 0,
+        }
+        await client.post(CHECKIN_URL, data=data)
+        logger.info(f"Checkin: {username} Checkin...")
+        home = await client.get(DK_URL)
+        soup = BeautifulSoup(home.content, "lxml")
+        return (
+            True
+            if "success" in str(soup.find("div", attrs={"class": "form-group"}))
+            else False
+        )
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
     }
-    post = s.post(home.url, data=data)
-    logger.info("Login: Login...")
-    return True if post.url == DK_URL else False
+    async with httpx.AsyncClient(headers=headers, timeout=None, verify=False) as client:
+        for i in range(retry_count):
+            result = await login(client, username, password)
+            if result:
+                logger.info("Login: Successful!")
+                break
+            else:
+                logger.warning(f"Login: {i} Try")
+        for i in range(retry_count):
+            result = await checkin(client, username, region)
+            if result:
+                logger.info("Checkin: Successful!")
+                break
+            else:
+                logger.warning(f"Checkin: {i} Try")
 
 
-def checkin():
-    data = {
-        "xgh": USERNAME,
-        "lon": "",
-        "lat": "",
-        "region": 1,
-        "rylx": 4,
-        "status": 0,
-    }
-    post = s.post(CHECKIN_URL, data=data)
-    data = {
-        "xgh": USERNAME,
-        "alwaysinsh": 1,
-        "fromaddr": "",
-        "fromtime": "",
-        "totime": "",
-        "jtgj": "",
-        "status": 0
-    }
-    post = s.post(ARRSH_URL, data=data)
-    logger.info("Checkin: Checkin...")
-    soup = BeautifulSoup(post.content, "lxml")
-    return (
-        True
-        if "success" in str(soup.find("div", attrs={"class": "form-group"}))
-        else False
-    )
+def load_json(filename="config.json"):
+    try:
+        with open(filename, "r") as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        try:
+            filename = f"{os.path.split(os.path.realpath(__file__))[0]}/{filename}"
+            with open(filename, "r") as file:
+                config = json.load(file)
+        except FileNotFoundError:
+            logger.exception(f"Cannot find {filename}.")
+            sys.exit(1)
+    logger.info(f"Json: Loaded {filename}")
+    return config
 
 
 if __name__ == "__main__":
-    USERNAME = sys.argv[1] if len(sys.argv) >= 2 else input("Input username:")
-    PASSWORD = sys.argv[2] if len(sys.argv) >= 3 else input("Input password:")
-    retry_count = 5
-    s = requests.Session()
-    s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'})
-    for i in range(retry_count):
-        result = login()
-        if result:
-            logger.info("Login: Successful!")
-            break
-        else:
-            logger.warning(f"Login: {i} Try")
-    for i in range(retry_count):
-        result = checkin()
-        if result:
-            logger.info("Checkin: Successful!")
-            break
-        else:
-            logger.warning(f"Checkin: {i} Try")
+    if len(sys.argv) >= 2 and os.path.exists(sys.argv[1]):
+        configs = load_json(sys.argv[1])
+    else:
+        configs = {
+            "USERS": [
+                {
+                    "USERNAME": input("学号: "),
+                    "PASSWORD": input("密码: "),
+                    "REGION": int(input("地区: ") or 1),
+                }
+            ]
+        }
+    loop = uvloop.new_event_loop()
+    scheduler = AsyncIOScheduler(event_loop=loop)
+    scheduler.start()
+    for i, config in enumerate(configs.get("USERS", [])):
+        job = scheduler.add_job(
+            user,
+            "cron",
+            args=[
+                config.get("USERNAME"),
+                config.get("PASSWORD"),
+                config.get("REGION", 1),
+            ],
+            name=config.get("USERNAME"),
+            hour=0,
+            minute=2 + i,
+            jitter=30,
+        )
+        logger.info(job.next_run_time)
+    loop.run_forever()
